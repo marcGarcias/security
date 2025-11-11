48dee818975f06c5015c5b3528c8dd699f2fb364
Marcelo Garcias <garcias.marcelo98@proton.me>
2025-11-10 23:48:06 -0300

    feat: Create the users table using Thymeleaf with name, email, and password.

---
+++ src/main/resources/db/migration/V1__create_table_users.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
