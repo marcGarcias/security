4f2e9a25d5b3dd39c77fe132ae0f84f4710f8fc2
Marcelo Garcias <garcias.marcelo98@proton.me>
2025-11-10 23:09:53 -0300

    feat: Create the UserRepository.

--- src/main/java/dev/garcias/security/repository/UserRepository.java
+++ src/main/java/dev/garcias/security/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package dev.garcias.security.repository;
 
-public interface UserRepository {
+import dev.garcias.security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<UserDetails> findUserByEmail(String userName);
 }
